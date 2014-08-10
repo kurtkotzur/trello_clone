b3ea34d04a3d75b24d64a50ac1cd13f5d01603b8
App Academy Student <student@appacademy.io>
2014-08-09 17:24:21 -0700

    restyled

---
+++ app/assets/javascripts/utils/util.js
@@ -0,0 +1,6 @@
+$( function () {
+  $("#guest-button").on("click", function (event) {
+    $("#email-input").val("kurtgeorge@gmail.com");
+    $("#password-input").val("password");
+  })
+});
--- app/assets/javascripts/views/list_show.js
+++ app/assets/javascripts/views/list_show.js
@@ -1,40 +1,40 @@
 TrelloClone.Views.ListShow = Backbone.CompositeView.extend({
-  className: "list list-group-item",
-  
+  className: "list panel panel-default",
+
   template: JST["list_show"],
-  
+
   id: function () {
     return this.model.get("id");
   },
-  
+
   events: {
     "click .delete-list": "deleteList",
     "click .new-card-button": "showForm",
     // "mouseenter": "handleMouseOver",
 //     "mouseleave": "handleMouseOver"
   },
-  
+
   initialize: function () {
     this.listenTo(this.model, "sync", this.render);
     this.listenTo(this.model.cards(), "add", this.addCard);
-    
+
     var cardNewView = new TrelloClone.Views.CardNew({ model: this.model });
     this.addSubview(".card-new", cardNewView);
-    
+
     this.model.cards().each(this.addCard.bind(this));
   },
-  
+
   addCard: function (card) {
     var cardShow = new TrelloClone.Views.CardShow({ model: card });
     this.addSubview(".cards", cardShow);
   },
-  
+
   deleteList: function (event) {
     event.preventDefault();
     this.model.destroy();
     this.remove();
   },
-  
+
   render: function () {
     var renderedContent = this.template({ list: this.model })
     this.$el.html(renderedContent);
@@ -44,18 +44,18 @@ TrelloClone.Views.ListShow = Backbone.CompositeView.extend({
     var activeCard;
     var that = this;
     $(".cards").sortable({
-      
+
       connectWith: ".cards",
-      
+
       start: function (event, ui) {
         activeCard = ui.item;
         fromList = $(event.target).parent();
       },
-      
+
       over: function (event, ui) {
         toList = $(event.target).parent();
       },
-      
+
       update: function (event, ui) {
         if (fromList !== toList) {
           fromList.cards().remove(activeCard);
@@ -64,16 +64,16 @@ TrelloClone.Views.ListShow = Backbone.CompositeView.extend({
         var data = $(this).sortable("toArray");
         //same as in board_show.js
       }
-      
+
     });
-    
+
     return this;
   },
-  
+
   handleMouseOver: function (event) {
     $(event.currentTarget).find(".delete-list").toggleClass("hidden");
   },
-  
+
   showForm: function (event) {
     $(event.currentTarget).addClass("hidden");
     this.$('.card-new').removeClass('hidden');
